494b33accba02678902f9ee697a107977b9e3f1a
Lovis Tawiah <69266504+lovistawiah@users.noreply.github.com>
2023-05-29 07:37:51 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+if (require("electron-squirrel-startup")) app.quit()
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+    ipcMain.handle('ping', () => 'pong')
+    win.loadFile('index.html')
+}
+app.whenReady().then(createWindow)
